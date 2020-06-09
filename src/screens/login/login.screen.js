d52443a266531d33b62608a8449f4a3d0ce72838
alphakongx <alphakongx1@gmail.com>
2020-06-09 07:41:08 -0700

    setup screens

---
+++ src/screens/login/login.screen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+import styles from "./login.style.js";
+
+const LoginScreen: () => React$Node = props => {
+  return <View style={styles.container} />;
+};
+
+export default LoginScreen;
