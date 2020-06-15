b61877339bff160fdfe20208565ebf8fc8a33e13
alphakongx <alphakongx1@gmail.com>
2020-06-15 13:12:54 -0700

    add signup birthdate screen

---
+++ src/screens/signup-birthdate/signup-birthdate.style.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backButtonContainer: {
+    width: 35,
+    height: 35,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 37,
+  },
+  titleFieldContainer: {
+    flex: 1,
+    paddingTop: 120,
+  },
+  titleText: {
+    fontSize: 24,
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 50,
+  },
+  inputFieldSeparator: {
+    marginBottom: 24,
+  },
+  buttonContainer: {
+    marginTop: 40,
+  },
+});
