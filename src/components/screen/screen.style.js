ef748ded2aba5110c404b9ec0a156c239e0eb81c
Abdul Meher <abdulmeher0823@gmail.com>
2020-06-23 23:42:05 -0700

    fix background image on Android

--- src/components/screen/screen.style.js
+++ src/components/screen/screen.style.js
@@ -1,13 +1,11 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {
+  imageBackground: {
     flex: 1,
   },
-  imageBackground: {
-    height: "100%",
-    width: "100%",
-    backgroundColor: "white",
+  container: {
+    flex: 1,
   },
 });
 
