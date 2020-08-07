652f6db592503395525fb5d230cbca86c000e94a
alphakongx <alphakongx1@gmail.com>
2020-08-07 11:26:19 -0700

    react version 0.62.2

---
+++ src/components/wheel-date-picker/wheel-date-picker.js
@@ -0,0 +1,184 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  View,
+  Dimensions,
+} from 'react-native';
+import moment from "moment";
+import { ScrollPicker } from "../wheel-scroll";
+import styles from "./wheel-date-picker.style";
+
+const { width } = Dimensions.get("window");
+
+export default class WheelDatePicker extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { date, minYear } = props;
+    
+    this.state = {
+      date,
+      monthRange: ["January", "Feburay", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+      yearRange: [] 
+    };
+
+    this.newValue = {};
+
+    this.parseDate(this.state.date);
+
+    const mdate = moment(date);
+
+    const dayNum = mdate.daysInMonth();
+    this.state.dayRange = this.genDateRange(dayNum);
+    
+    for (let i = minYear; i <= mdate.year(); i += 1) {
+      this.state.yearRange.push(i);
+    }
+  }
+
+  parseDate = (date) => {
+    const mdate = moment(date);
+
+    ['year', 'month', 'date', 'hour', 'minute'].forEach((s) => { this.newValue[s] = mdate.get(s); });
+  }
+
+  genDateRange(dayNum) {
+    const days = [];
+
+    for (let i = 1; i <= dayNum; i += 1) {
+      days.push(i);
+    }
+
+    return days;
+  }
+
+  onYearChange = (year) => {
+    const oldYear = this.newValue.year;
+
+    this.newValue.year = year;
+    this.checkDate(oldYear, this.newValue.month);
+    if (this.props.onDateChange) {
+      this.props.onDateChange(this.getValue());
+    }
+  };
+
+  onMonthChange = (month) => {
+    const oldMonth = this.newValue.month;
+
+    this.newValue.month = month;
+    this.checkDate(this.newValue.year, oldMonth);
+    if (this.props.onDateChange) {
+      this.props.onDateChange(this.getValue());
+    }
+  };
+
+  onDateChange = (date) => {
+    this.newValue.date = date;
+    this.checkDate(this.newValue.year, this.newValue.month);
+    if (this.props.onDateChange) {
+      this.props.onDateChange(this.getValue());
+    }
+  };
+
+  checkDate(oldYear, oldMonth) {
+    const currentMonth = this.newValue.month;
+    const currentYear = this.newValue.year;
+    const currentDay = this.newValue.date;
+
+    let dayRange = this.state.dayRange;
+    let dayNum = dayRange.length;
+
+    if (oldMonth !== currentMonth || oldYear !== currentYear) {
+      dayNum = moment(`${currentYear}-${currentMonth + 1}`, 'YYYY-MM').daysInMonth();
+    }
+
+    if (dayNum !== dayRange.length) {
+      dayRange = this.genDateRange(dayNum);
+
+      if (currentDay > dayNum) {
+        this.newValue.date = dayNum;
+      }
+
+      this.setState({ dayRange });
+    }
+  }
+
+  getValue() {
+    const { year, month, date, hour, minute } = this.newValue;
+    const nextDate = new Date(year, month, date, hour, minute);
+
+    if (nextDate < this.props.minimumDate) {
+      return this.props.minimumDate;
+    }
+
+    return nextDate > this.props.maximumDate ? this.props.maximumDate : nextDate;
+  }
+
+  render() {
+    const { dayRange, monthRange, yearRange } = this.state;
+    return (
+      <View style={styles.container}>
+        <ScrollPicker
+          ref={(ref) => { this.dateComponent = ref; }}
+          dataSource={dayRange}
+          selectedIndex={dayRange.indexOf(this.newValue.date)}
+          wrapperHeight={178}
+          wrapperWidth={width * 22 / 100}
+          wrapperBackground={"transparent"}
+          itemHeight={46}
+          highlightColor={"white"}
+          highlightWidth={width * 22 / 100}
+          highlightBorderWidth={2}
+          itemTextStyle={styles.dateText}
+          activeItemTextStyle={styles.activeDateText}
+          onValueChange={(value, index) => {
+            this.onDateChange(value);
+          }}
+        />
+        <View style={styles.spacerView} />
+        <ScrollPicker 
+          dataSource={monthRange}
+          selectedIndex={this.newValue.month}
+          wrapperHeight={180}
+          wrapperWidth={width * 27 / 100}
+          wrapperBackground={"transparent"}
+          itemHeight={50}
+          highlightColor={"white"}
+          highlightWidth={width * 27 / 100}
+          highlightBorderWidth={2}
+          itemTextStyle={styles.dateText}
+          activeItemTextStyle={styles.activeDateText}
+          onValueChange={(value, index) => {
+            this.onMonthChange(index);
+          }}
+        />
+        <View style={styles.spacerView} />
+        <ScrollPicker 
+          dataSource={yearRange}
+          selectedIndex={yearRange.indexOf(this.newValue.year)}
+          wrapperHeight={180}
+          wrapperWidth={width * 22 / 100}
+          wrapperBackground={"transparent"}
+          itemHeight={50}
+          highlightColor={"white"}
+          highlightWidth={width * 22 / 100}
+          itemTextStyle={styles.dateText}
+          activeItemTextStyle={styles.activeDateText}
+          onValueChange={(value, index) => {
+            this.onYearChange(value);
+          }}
+        />
+      </View>
+    )
+  }
+}
+
+WheelDatePicker.propTypes = {
+  minYear: PropTypes.number,
+  onDateChange: PropTypes.func,
+};
+
+WheelDatePicker.defaultProps = {
+  minYear: 1945,
+  onDateChange: null,
+};
---
+++ src/screens/profile-images-reorder/profile-images-reorder.js
@@ -0,0 +1,200 @@
+import React from "react";
+import { View, Dimensions, ScrollView, ActivityIndicator } from "react-native";
+import { SafeAreaView } from "react-navigation";
+import { Screen, Touchable, Image, Text, DragSortableView } from "@components";
+import ImagePicker from "react-native-image-picker";
+import LinearGradient from "react-native-linear-gradient";
+import EventBus from "eventing-bus";
+import { UserTypes } from "@redux/actions";
+import { Notification } from "@helpers";
+import { GRADIENT } from "@config";
+import Images from "@assets/Images";
+import styles from "./profile-images-reorder.style";
+
+const { width } = Dimensions.get("window");
+
+class OrderImages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      uploading: false,
+      scrollEnabled: true,
+    }
+  }
+
+  componentDidMount() {
+    this.updateSuccessAction = EventBus.on(
+      UserTypes.UPDATE_USER_SUCCESS,
+      this.uploadingDone,
+    );
+    this.updateFailureAction = EventBus.on(
+      UserTypes.UPDATE_USER_FAILURE,
+      this.uploadingDone,
+    );
+  }
+
+  componentWillUnmount() {
+    this.updateSuccessAction();
+    this.updateFailureAction();
+  }
+
+  uploadingDone = () => {
+    this.setState({ uploading: false });
+  };
+
+  onAddNewImage = () => {
+    const options = {
+      title: "Select Image",
+      storageOptions: {
+        skipBackup: true,
+        path: "images",
+      },
+    };
+
+    ImagePicker.showImagePicker(options, response => {
+      if (response.didCancel) {
+      } else if (response.error) {
+      } else if (response.customButton) {
+      } else {
+        let photoUriSplit = response.uri.split("/");
+
+        const image = {
+          uri: response.uri,
+          name: photoUriSplit[photoUriSplit.length - 1],
+          type: response.type,
+        };
+        this.setState({ uploading: true }, () => {
+          const params = new FormData();
+          params.append("images[]", image);
+          this.props.updateUser(params, this.props.token);
+        });
+      }
+    });
+  }
+
+  onDeleteImage = imageId => {
+    if (this.props.user.images.length === 1) return;
+    Notification.confirmAlert("Delete", "Do you want to delete this image?", () => {
+      this.props.deleteImage(imageId, this.props.token);
+    });
+  };
+
+  onDragStart = () => {
+    this.setState({
+      scrollEnabled: false
+    });
+  };
+
+  onDragEnd = (startIndex, endIndex) => {
+    this.setState({
+      scrollEnabled: true
+    });
+  };
+
+  render() {
+    const { uploading } = this.state;
+    let showDelete = this.props.user.images.length > 1;
+
+    let imageData = this.props.user.images;
+    if (uploading) {
+      imageData = [...this.props.user.images, {id: -1}];
+    }
+
+    return (
+      <Screen>
+        <SafeAreaView style={styles.container}>
+        <ScrollView 
+          bounces={false}
+          scrollEnabled={this.state.scrollEnabled}
+          style={styles.container}>
+          <View style={styles.headerContainer}>
+            <Touchable
+              style={styles.backButton}
+              onPress={() => {
+                this.props.navigation.goBack();
+              }}
+            >
+              <Image source={Images.app.icBackLeft} style={styles.backButtonIcon} />
+            </Touchable>
+            <Text style={styles.titleText}>
+              {`${this.props.user.images.length} Images`}
+            </Text>
+          </View>
+
+          <View style={styles.dragContainer}>
+            <DragSortableView
+              dataSource={imageData}
+              parentWidth={width}
+              childrenWidth={(width - 80) / 3}
+              childrenHeight={(width - 80) / 2}
+              marginChildrenBottom={15}
+              marginChildrenLeft={10}
+              marginChildrenRight={10}
+              keyExtractor={(item, index) => item.id}
+              renderItem={(item, index) => {
+                if (item.id === -1) {
+                  return (
+                    <View style={styles.uploadingContainer}>
+                      <ActivityIndicator size={"small"} color={"white"} />
+                    </View>
+                  )
+                }
+                return (
+                  <View style={styles.itemContainer}>
+                    <Image source={{ uri: item.path }} style={styles.image} />
+                    {
+                      showDelete &&
+                      <Touchable style={styles.deleteContainer}
+                        onPress={() => this.onDeleteImage(item.id)}>
+                        <Text style={styles.deleteText}>X</Text>
+                      </Touchable>
+                    }
+                    {
+                      this.props.deletingImageId === item.id && (
+                        <View style={styles.imageLoadingContainer}>
+                          <ActivityIndicator size={"small"} color={"white"} />
+                        </View>
+                      )
+                    }
+                  </View>
+                )
+              }}
+              onDragStart={this.onDragStart}
+              onDragEnd={this.onDragEnd}
+              onDataChange={data => {
+                if (this.state.uploading) return;
+                let params = new FormData();
+                for (let i=0; i<data.length; i++) {
+                  data[i].sort = i + 1;
+                  params.append(`sort[${data[i].id}]`, data[i].sort);
+                }
+                let user = this.props.user;
+                user.images = data;
+                this.props.updateUserImage(user);
+                this.props.reorderImages(params, this.props.token);
+              }}
+            />
+          </View>
+          <Text style={styles.hintText}>{"Long press to reorder"}</Text>
+        </ScrollView>
+        
+        <View style={styles.favContainer}>
+          <Touchable 
+            onPress={this.onAddNewImage}>
+            <LinearGradient
+              colors={GRADIENT.BUTTON}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 0, y: 1 }}
+              style={styles.plusFav}
+            >
+              <Image source={require("@assets/images/live-screen/plus-fav.png")} />
+            </LinearGradient>
+          </Touchable>
+        </View>
+        </SafeAreaView>
+      </Screen>
+    );
+  }
+}
+
+export default OrderImages;
